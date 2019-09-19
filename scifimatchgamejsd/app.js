b28696e4e9805316107b91c61124d56038ee2e30
James White <jimernest90@yahoo.com>
2019-09-19 17:00:19 -0400

    made hover function and click event

--- scifimatchgamejsd/app.js
+++ scifimatchgamejsd/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $(".boxTop").click(function(){
-    console.log('you have clicked my son')
+    //console.log('you have clicked my son')
     $(event.currentTarget).css("background-color", "#42f5a7")
   })
   $(".boxBottom").click(function(){
-    console.log('you have clicked my daughter')
+    //console.log('you have clicked my daughter')
     $(event.currentTarget).css("background-color", "#42f5a7")
   })
 })
