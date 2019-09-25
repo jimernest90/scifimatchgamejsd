e32ee2b406d0dc03dbd2295ec9537c949cb5591b
James White <jimernest90@yahoo.com>
2019-09-25 09:30:25 -0400

    changed htmllayout

--- scifimatchgamejsd/app.js
+++ scifimatchgamejsd/app.js
@@ -1,40 +1,16 @@
 $(document).ready(function() {
 //randomize $boxes
-  const $container = $('.container'),
-    $boxes = $container.children();
+  const $grid = $('.grid'),
+    $boxes = $grid.children();
     //defines $boxes as the children of the container
     $boxes.sort(function() {return Math.random() - .5; });
     // update dom with new sort
-    $container.append($boxes);
+    $grid.append($boxes);
 
     let firstGuess = ''
-    let secondGuess =''
-    let count= 0
-//add click function that turns the border blue
-  $boxes.click(function(){
-  //...
-  if (count < 2) {
-    count++
-    if (count === 1) {
-      // Assign first guess
-      firstGuess = $boxes.name
+    let secondGuess = ''
+    let count = 0
 
-      $(event.currentTarget).css('border', '2px solid blue')
-    } else {
-      // Assign second guess
-      secondGuess = $boxes.name
 
-      $(event.currentTarget).css('border', '2px solid blue')
-    }
-    }
 
-    // If both guesses are not empty...
-    if (firstGuess !== '' && secondGuess !== '') {
-      // and the first guess matches the second match...
-      if (firstGuess === secondGuess) {
-        // run the match function
-        
-      }
-    }
-    })
 })
